3a164a537ae75d06f61c19d006f3407d66f62f64
redssy <redlemagic@hotmail.com>
2024-06-25 18:48:37 -0300

    feat: subindo ex

--- exercicio_bootstrap/fontes_js/index.js
+++ exercicio_bootstrap/fontes_js/index.js
@@ -12,34 +12,55 @@ let InicializaJQuery = function(){
         console.log('JQuery inicializado.');
         /////////////////
         flgJQuery = true;
-
-        
-    let enviarCadastro = function(e){
-        try {
-            // valida JQuery pronto
-            if (flgJQuery) {
-                //apresenta mensagem
-                e.preventDefault();
-                // exibe mensagem
-                alert('Cadastro enviado.');
-                // recarrega pagina
-                window.location.reload(true);
-            }
-        } catch (error) {
-            console.log(error.message)
-        }
-    }
-
-
-
-    //eventos
-    $j('button').on('click', enviarCadastro)
     } catch (error) {
         flgJQuery = false;
         console.log(error.message);
     }
 }
 
+let enviarCadastro = function(e){
+    try {
+        // valida JQuery pronto
+        if (flgJQuery) {
+            //apresenta mensagem
+            e.preventDefault();
+             // correcao 6: atributo 'requerido' nao existe, e sim 'required'
+            $('form').validate({
+                rules: {
+                    strNome: {
+                        required: true,
+                        placeholder: 'Seu nome'
+                    },
+                    strEmail: {
+                        required: true,
+                        email: true,
+                        placeholder: 'Seu email'
+                    },
+                    strContato: {
+                        placeholder: 'Seu contato'
+                    }
+                },
+                submitHandler: function (form) {
+                    alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+                     // recarrega pagina
+                    window.location.reload(true);
+                },
+                invalidHandler: function (form, validator) {
+                    // alteracao 7: exibicao da quantidade de campos invalidos
+                    alert("Por favor, preencha os campos para prosseguir com a compra! " + validator.numberOfInvalids() + ' campos inválidos!');
+                }
+            })
+        }
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+
+
 
 //evento p/ validar JQuery
 $j(document).ready(InicializaJQuery);
+//eventos
+$j('form').on('submit', enviarCadastro)
+
