2eeb64d9d4e21d9c1234dcd4115c6a8e74a6ad5e
redssy <redlemagic@hotmail.com>
2024-06-25 18:32:18 -0300

    feat: subindo ex

--- exercicio_bootstrap/fontes_js/index.js
+++ exercicio_bootstrap/fontes_js/index.js
@@ -1,3 +1,6 @@
+// ativa modo sem comfl
+let $j = jQuery.noConflict();
+
 //== variaveis
 //controle
 let flgJQuery = false;
@@ -9,13 +12,34 @@ let InicializaJQuery = function(){
         console.log('JQuery inicializado.');
         /////////////////
         flgJQuery = true;
+
+        
+    let enviarCadastro = function(e){
+        try {
+            // valida JQuery pronto
+            if (flgJQuery) {
+                //apresenta mensagem
+                e.preventDefault();
+                // exibe mensagem
+                alert('Cadastro enviado.');
+                // recarrega pagina
+                window.location.reload(true);
+            }
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+
+
+    //eventos
+    $j('button').on('click', enviarCadastro)
     } catch (error) {
         flgJQuery = false;
         console.log(error.message);
     }
 }
 
-//evento p/ validar JQuery
-$(document).ready(InicializaJQuery);
 
-//eventos
+//evento p/ validar JQuery
+$j(document).ready(InicializaJQuery);
