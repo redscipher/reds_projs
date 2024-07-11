7aa736fc11ceb6a46ac8f4bc1f04e4b484f8b2d7
redssy <redlemagic@hotmail.com>
2024-07-11 19:46:38 -0300

    feat: subindo validacoes

--- exercicio_gulp/src/js/index.js
+++ exercicio_gulp/src/js/index.js
@@ -16,10 +16,32 @@ let inicializaBootstrap = function(){
 
 let inicializaJQuery = function(){
      try {
-         console.log('JQuery preparado');
-         // carregado
-         flgJQuery = true
-         console.log(flgJQuery);
+          console.log('JQuery preparado');
+          // carregado
+          flgJQuery = true
+          console.log(flgJQuery);
+
+          // correcao 5: 'mascara' nao existe, e sim 'mask'
+          $('#id-link').mask('(00) 00000-0000', {
+               placeholder: 'https://website.com'
+          })
+      
+          // correcao 6: atributo 'requerido' nao existe, e sim 'required'
+          $('form').validate({
+               rules: {
+                    nmLink: {
+                         required: true
+                    }
+               },
+               submitHandler: function (form) {
+                    alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+               form.reset();
+               },
+               invalidHandler: function (form, validator) {
+               // alteracao 7: exibicao da quantidade de campos invalidos
+                    alert("Por favor, preencha os campos para prosseguir com a compra! " + validator.numberOfInvalids() + ' campos inválidos!');
+               }
+          })
     } catch (error) {
           flgJQuery = false
           console.log(error.message);
@@ -30,7 +52,7 @@ let inicializaJQuery = function(){
      try {
           // valida jquery
           if (flgJQuery) {
-               
+
           }
      } catch (error) {
           console.log(error.message);
@@ -54,6 +76,8 @@ let inicializaJQuery = function(){
           console.log(error.message);
      }
  }
+
+
 
 
 // confirma/espera todo conteudo do dom ser carregado
