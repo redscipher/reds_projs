60c0dced2f64eb5d4cc3541bccebbbc4efea7754
redssy <redlemagic@hotmail.com>
2024-07-12 11:06:10 -0300

    chore: corrigindo leiautes + adicao de imagens

--- exercicio_gulp/src/js/index.js
+++ exercicio_gulp/src/js/index.js
@@ -20,28 +20,6 @@ let inicializaJQuery = function(){
           // carregado
           flgJQuery = true
           console.log(flgJQuery);
-
-          // correcao 5: 'mascara' nao existe, e sim 'mask'
-          $('#id-link').mask('(00) 00000-0000', {
-               placeholder: 'https://website.com'
-          })
-      
-          // correcao 6: atributo 'requerido' nao existe, e sim 'required'
-          $('form').validate({
-               rules: {
-                    nmLink: {
-                         required: true
-                    }
-               },
-               submitHandler: function (form) {
-                    alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-               form.reset();
-               },
-               invalidHandler: function (form, validator) {
-               // alteracao 7: exibicao da quantidade de campos invalidos
-                    alert("Por favor, preencha os campos para prosseguir com a compra! " + validator.numberOfInvalids() + ' campos inválidos!');
-               }
-          })
     } catch (error) {
           flgJQuery = false
           console.log(error.message);
@@ -50,34 +28,79 @@ let inicializaJQuery = function(){
 
  let validaEntrada = function(){
      try {
+          // variavel
+          let controle = false;
           // valida jquery
           if (flgJQuery) {
-
+               // valida formulario
+               $('form').validate({
+                    rules: {
+                         nmLink: {
+                              required: true
+                         }
+                    },
+                    submitHandler: function (form) {
+                         adicionaImagem()
+                         // reseta formulario
+                         form.reset();
+                    },
+                    invalidHandler: function (form, validator) {
+                         alert("Por favor, preencha o campo adicionar uma imagem! " + validator.numberOfInvalids() + ' campos inválidos!');
+                    }
+               })
           }
+          // def retorno
+          return controle;
      } catch (error) {
           console.log(error.message);
      }
  }
 
- let adicionaImagem = function(e){
+ let validaAdcImagem = function(e){
      try {
+          e.preventDefault();
           // valida jquery e bootstrap
           if (flgBootstrap && flgJQuery) {
-               alert('voce apertou o botao');
                // valida entrada correta
-               if (validaEntrada()) {
-
-               }else{
-                    // lanca excecao
-
-               }
+               validaEntrada();
           }
      } catch (error) {
           console.log(error.message);
      }
  }
 
-
+let adicionaImagem = function(){
+     try {
+          // pega carrossel
+          const carrossel = $('#id-carrossel');
+          // validacao
+          if (carrossel){
+               // constante
+               const eLink = $('#id-link');
+               let strEndereco = eLink.val();
+               // adciona novo elemento na lista
+               const novoItem = $('<li class="carousel-item" data-bs-interval="3000"></li>');
+               // imagem + efetua adicao de fato com metodo appendTo()
+               $(`<img class="d-block w-100" src="${strEndereco}" alt="${strEndereco}">`).appendTo(novoItem);
+               //efetua insercao pelo metodo append()
+               $('ul').append(novoItem)
+               // reseta campo
+               eLink.val('');
+               // itens do carrosel
+               const items = document.querySelectorAll('.carousel-item');
+               // tamanho do carrossel
+               let tamCarr = items.length - 1;
+               // novo indicador p/ adicionar
+               const novoIndicador = $(`<button type="button" data-bs-target="#id-carrossel" data-bs-slide-to="${tamCarr}"></button>`);
+               // adiciona no objeto indicador
+               $('.carousel-indicators').append(novoIndicador);
+               // Atualize o carousel
+               carrossel.carousel('next');
+          }
+     } catch (error) {
+          alert(error.message);
+     }
+}
 
 
 // confirma/espera todo conteudo do dom ser carregado
@@ -85,4 +108,4 @@ document.addEventListener('DOMContentLoaded', inicializaBootstrap);
 $(document).ready(inicializaJQuery);
 
 // eventos
-$('form').on('click', adicionaImagem);
+$('form').on('submit', validaAdcImagem);
