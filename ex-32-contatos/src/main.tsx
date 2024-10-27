a2e25eaf1442e24957f7da09cfe22d05747cede5
redssy <redlemagic@hotmail.com>
2024-10-27 13:38:55 -0300

    resetando app

--- ex-32-contatos/src/main.tsx
+++ ex-32-contatos/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+// icones bootstrap
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+// importando react
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
